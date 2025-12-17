c741808c1a698ffdf3ad6e6d66647efb5e66e2c3
AhsanDeveloper764 <ahsanalise226@gmail.com>
2025-12-18 03:00:24 +0500

    Cod Start

---
+++ Vue Frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <SignUp/>
+</template>
+<script >
+  import SignUp from './Components/SignUp.vue';
+  export default {
+    name:"App",
+    components:{
+      SignUp
+    }
+  }
+</script>
+<style scoped>
+
+</style>
---
+++ Vue Frontend/src/Components/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="main" >
+        <img src="../Assets/logo.jpg" alt="img-logo" class="img-logo" >
+        <h1 class="text-signUp">Sign Up Page</h1>
+    </div>
+</template>
+
+<script >
+    export default {
+        name:"SignUp"
+    }
+</script>
+
+<style scoped>
+    .main{
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+        line-height: 0px;
+        margin: 0 auto;
+    }
+    .img-logo{
+        width: 10%;
+    }
+    .text-signUp{
+        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-size: 30px;
+    }
+</style>
